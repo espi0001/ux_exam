5baa499f74d71e200eeb658f0c53e8779a2c75ec
Ester <espi0001@stud.kea.dk>
2025-12-15 15:34:45 +0100

    Update navigation across all pages to include a home link and adjust cart visibility based on user login status; refine CSS for consistent padding on body element.

--- js/loginCheck.js
+++ js/loginCheck.js
@@ -6,15 +6,17 @@ const email = localStorage.getItem(LOCAL_STORAGE_USER_EMAIL);
 
 // check if user is logged in
 if (email !== null) {
-  // user IS logged in > hide login + signup and show logout button
+  // user IS logged in > hide login + signup and show logout button + cart
   document.querySelector("#login").classList.add("hidden");
   document.querySelector("#signup").classList.add("hidden");
   document.querySelector("#logout").classList.remove("hidden");
+  document.querySelector("#cart").classList.remove("hidden");
 } else {
-  // user is NOT logged in > show login + signup and hide logout
+  // user is NOT logged in > show login + signup and hide logout + cart
   document.querySelector("#login").classList.remove("hidden");
   document.querySelector("#signup").classList.remove("hidden");
   document.querySelector("#logout").classList.add("hidden");
+  document.querySelector("#cart").classList.add("hidden");
 }
 
 // når man logger ud skal man ikke kunne se noget i cart, men cart skal ikke blive 0
